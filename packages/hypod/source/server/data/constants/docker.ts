78115e7bcf4368a23ae8befeb1ba032c1d3330d3
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-08-09 10:07:54 +0300

    hypod > dev: server

--- packages/hypod/source/server/data/constants/docker.ts
+++ packages/hypod/source/server/data/constants/docker.ts
@@ -1,3 +1,5 @@
 export const DOCKER_ENDPOINT_IGNORE = '/v2/*';
 
 export const DOCKER_ENDPOINT_API_VERSION_CHECK = '/v2';
+export const DOCKER_ENDPOINT_API_CATALOG = '/v2/_catalog';
+export const DOCKER_ENDPOINT_API_ALL = '/v2/*';
--- packages/hypod/source/server/handlers/docker.ts
+++ packages/hypod/source/server/handlers/docker.ts
@@ -2,6 +2,8 @@ import express from 'express';
 
 import {
     DOCKER_ENDPOINT_API_VERSION_CHECK,
+    DOCKER_ENDPOINT_API_CATALOG,
+    DOCKER_ENDPOINT_API_ALL,
 } from '#server/data/constants';
 
 
@@ -14,6 +16,56 @@ const endpointApiVersionCheck = (
     response.status(200).end();
 }
 
+const endpointApiGetAll = (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction,
+) => {
+    console.log(request.originalUrl);
+
+    response.status(200).end();
+}
+
+const endpointApiPostAll = (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction,
+) => {
+    console.log(request.originalUrl);
+
+    response.status(200).end();
+}
+
+const endpointApiPutAll = (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction,
+) => {
+    console.log(request.originalUrl);
+
+    response.status(200).end();
+}
+
+const endpointApiPatchAll = (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction,
+) => {
+    console.log(request.originalUrl);
+
+    response.status(200).end();
+}
+
+const endpointApiDeleteAll = (
+    request: express.Request,
+    response: express.Response,
+    next: express.NextFunction,
+) => {
+    console.log(request.originalUrl);
+
+    response.status(200).end();
+}
+
 
 const dockerHandlers = (
     instance: express.Application,
@@ -21,6 +73,36 @@ const dockerHandlers = (
     instance.get(
         DOCKER_ENDPOINT_API_VERSION_CHECK,
         endpointApiVersionCheck,
+    );
+
+    instance.get(
+        DOCKER_ENDPOINT_API_CATALOG,
+        endpointApiGetAll,
+    );
+
+    instance.get(
+        DOCKER_ENDPOINT_API_ALL,
+        endpointApiGetAll,
+    );
+
+    instance.post(
+        DOCKER_ENDPOINT_API_ALL,
+        endpointApiPostAll,
+    );
+
+    instance.put(
+        DOCKER_ENDPOINT_API_ALL,
+        endpointApiPutAll,
+    );
+
+    instance.patch(
+        DOCKER_ENDPOINT_API_ALL,
+        endpointApiPatchAll,
+    );
+
+    instance.delete(
+        DOCKER_ENDPOINT_API_ALL,
+        endpointApiDeleteAll,
     );
 }
 
