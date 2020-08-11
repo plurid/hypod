2f52a8cb3da5eea1ae960a38610a4e26621ef135
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-08-11 15:27:09 +0300

    hypod > dev: server/data

--- packages/hypod/source/server/data/constants/storage.ts
+++ packages/hypod/source/server/data/constants/storage.ts
@@ -14,8 +14,8 @@ import {
 // #region module
 export const BASE_PATH_DATA = '/data/';
 export const BASE_PATH_IMAGENES = '/imagenes/';
-export const BASE_PATH_IMAGENES_MANIFEST = '/imagenes/manifest';
-export const BASE_PATH_IMAGENES_SHA256 = '/imagenes/sha256';
+export const BASE_PATH_IMAGENES_MANIFEST = '/imagenes/manifest/';
+export const BASE_PATH_IMAGENES_SHA256 = '/imagenes/sha256/';
 export const BASE_PATH_BLOBS = '/blobs/';
 export const BASE_PATH_METADATA = '/metadata/';
 
