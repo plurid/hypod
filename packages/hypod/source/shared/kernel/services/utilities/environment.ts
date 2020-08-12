02bef96f44c2d3780e25d5912e788820c1763afb
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-08-12 22:36:58 +0300

    hypod > dev: shared/kernel

--- packages/hypod/source/shared/kernel/services/utilities/environment.ts
+++ packages/hypod/source/shared/kernel/services/utilities/environment.ts
@@ -4,8 +4,11 @@ const environment = {
     localExternal: process.env.ENV_MODE === 'localExternal',
     development: process.env.ENV_MODE === 'development',
     production: process.env.ENV_MODE === 'production',
+    customLogic: process.env.HYPOD_CUSTOM_LOGIC === 'true',
 };
 // #endregion module
+console.log('environment', environment);
+
 
 
 // #region exports
