59b9f287a9a17d8dd2fe48b8417827f7c0c0f5af
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-08-14 08:20:38 +0300

    hypod > dev: server | preserves

--- packages/hypod/source/server/preserves/index.ts
+++ packages/hypod/source/server/preserves/index.ts
@@ -4,12 +4,44 @@
         PluridPreserve,
     } from '@plurid/plurid-react';
     // #endregion libraries
+
+
+    // #region external
+    import {
+        PRIVATE_USAGE,
+    } from '#server/data/constants';
+    // #endregion external
 // #endregion imports
 
 
 
 // #region module
 const preserves: PluridPreserve<any>[] = [
+    {
+        serve: '*',
+        onServe: async (
+            transmission,
+        ) => {
+            const {
+                response,
+                context,
+            } = transmission;
+
+
+            if (PRIVATE_USAGE) {
+                // check if user is logged in
+                // console.log('context', context);
+
+                // response.status(404).end();
+
+                // return {
+                //     responded: true,
+                // };
+            }
+
+            return;
+        },
+    },
 ];
 
 
