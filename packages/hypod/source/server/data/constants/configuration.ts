f211c32f3e91707a99fac212351a0b984f859760
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-08-17 09:34:27 +0300

    hypod > setup: package

--- packages/hypod/source/server/data/constants/configuration.ts
+++ packages/hypod/source/server/data/constants/configuration.ts
@@ -10,7 +10,35 @@
 
 
 // #region module
-export const QUIET = process.env.HYPOD_QUIET ?? true;
+export const LOG_LEVEL = process.env.HYPOD_LOG_LEVEL || '7';
+export const QUIET = process.env.HYPOD_QUIET === 'true';
+
+
+export const LOG_LEVEL_NONE = 7;
+export const LOG_LEVEL_FATAL = 6;
+export const LOG_LEVEL_ERROR = 5;
+export const LOG_LEVEL_WARN = 4;
+export const LOG_LEVEL_INFO = 3;
+export const LOG_LEVEL_DEBUG = 2;
+export const LOG_LEVEL_TRACE = 1;
+export const LOG_LEVEL_ALL = 0;
+
+export const logLevels = {
+    none: LOG_LEVEL_NONE,
+    all: LOG_LEVEL_ALL,
+    fatal: LOG_LEVEL_FATAL,
+    error: LOG_LEVEL_ERROR,
+    warn: LOG_LEVEL_WARN,
+    info: LOG_LEVEL_INFO,
+    debug: LOG_LEVEL_DEBUG,
+    trace: LOG_LEVEL_TRACE,
+};
+
+export const logLevel = QUIET
+    ? 0
+    : parseInt(LOG_LEVEL);
+
+
 
 export const DATABASE_TYPE = (process.env.HYPOD_DATABASE_TYPE as DatabaseType | undefined)
     || 'filesystem';
