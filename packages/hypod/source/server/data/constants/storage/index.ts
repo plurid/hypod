a55b05d04e71ab49008d89b7c5c7e0396ab560d5
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-08-19 22:08:46 +0300

    hypod > dev: server/data

--- packages/hypod/source/server/data/constants/storage/index.ts
+++ packages/hypod/source/server/data/constants/storage/index.ts
@@ -18,9 +18,9 @@ export const BASE_PATH_IMAGENES_MANIFEST = '/imagenes/manifest/';
 export const BASE_PATH_IMAGENES_SHA256 = '/imagenes/sha256/';
 export const BASE_PATH_BLOBS = '/blobs/';
 export const BASE_PATH_METADATA = '/metadata/';
-export const BASE_PATH_METADATA_NAMESPACES = '/metadata/namespaces';
-export const BASE_PATH_METADATA_PROJECTS = '/metadata/projects';
-export const BASE_PATH_METADATA_IMAGENES = '/metadata/imagenes';
+export const BASE_PATH_METADATA_NAMESPACES = '/metadata/namespaces/';
+export const BASE_PATH_METADATA_PROJECTS = '/metadata/projects/';
+export const BASE_PATH_METADATA_IMAGENES = '/metadata/imagenes/';
 
 
 export const storageTypeFilesystem: StorageTypeFilesystem = 'filesystem';
